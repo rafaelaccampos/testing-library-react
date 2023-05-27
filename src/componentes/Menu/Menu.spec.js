c3aa1183c80b144266f6576def41447aa4450b4a
Rafaela Cassiano Campos <rafaela.cassiano@lambda3.com.br>
2023-05-26 23:50:12 -0300

    feat: Add menu tests

--- src/componentes/Menu/Menu.spec.js
+++ src/componentes/Menu/Menu.spec.js
@@ -3,20 +3,34 @@ import Menu from './index';
 
 test('Deve renderizar um link para a página inicial', () => {
   render(<Menu />);
+
   const linkPaginaInicial = screen.getByText('Inicial');
+
   expect(linkPaginaInicial).toBeInTheDocument();
 });
 
 test('Deve renderizar uma lista de links', () => {
   render(<Menu />);
+
   const listaDeLinks = screen.getAllByRole('link');
+
   expect(listaDeLinks).toHaveLength(4);
 });
 
-test('Não deve renderizar o link para Extrato', () => {
+test('Não deve renderizar o link para o extrato', () => {
   render(<Menu />);
+
   const linkExtrato = screen.queryByText('Extrato');
+
   expect(linkExtrato).not.toBeInTheDocument();
+});
+
+test('Deve renderizar uma lista de links com a classe link', () => {
+  render(<Menu />);
+
+  const links = screen.getAllByRole('link');
+
+  links.forEach((link) => expect(link).toHaveClass('links'));
 });
 
 test('Deve renderizar uma lista de links com a classe link', () => {
